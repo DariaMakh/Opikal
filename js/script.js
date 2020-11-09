c4d9d87ca9e7b9159bd124fef0507aa7f5ac03f2
daria <makhortova.daria@ya.ru>
2020-11-09 18:21:45 +0300

    Добавление блоков, слайдеры для страницы

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$('.possibility__slider_1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: true,
+    nextArrow: $('.possibility__next_1'),
+    prevArrow: $('.possibility__prev_1'),
+});
+
+$('.possibility__slider_2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: true,
+    nextArrow: $('.possibility__next_2'),
+    prevArrow: $('.possibility__prev_2'),
+});
