08d83516b87bac6589d3c90c925f9552dcc4ebf6
DariaMakh <DariaMakh@users.noreply.github.com>
2020-11-23 18:39:06 +0300

    Страница Предложения

--- js/script.js
+++ js/script.js
@@ -27,6 +27,27 @@ $('.offer__search_icon-2').click(function(event){
     $('.offer__advanced-search').toggleClass('active');
 });
 
+var select = function(){
+    var selectHeader = document.querySelectorAll('.select__header');
+    var selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach(item=>{
+        item.addEventListener('click', function(){
+            this.parentElement.classList.toggle('active');
+        });
+    });
+
+    selectHeader.forEach(item=>{
+        item.addEventListener('click', selectChoose);
+    });
+
+    function selectChoose(){
+        var selectItem = this.innerText;
+        currentItem = this.closest('.select__box').querySelector('selector__current')
+        console.log(selectItem);
+    };
+}
+
 
 // ОТКРЫТИЕ И ЗАКРЫТИЕ МОДАЛЬНОГО ОКНА
 
